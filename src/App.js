67a0d2a3427a06f07b5c015fe32a64f92f233d2f
patrickikhidero <80291797+patrickikhidero@users.noreply.github.com>
2022-02-25 11:21:32 +0100

    social app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/register" element={<Register/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
